6027bb5b53349034d4ebcc08714b657c09fdc26a
Rocketdude720 <am680781@yahoo.com>
2018-02-07 19:51:01 -0500

    Updated Snake.exe

--- Projects/Snake.java
+++ Projects/Snake.java
@@ -1,49 +1,43 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Snake extends Application{
 
     Stage window = new Stage();
     Scene launchScreen, gameScreen;
+
     public static void main(String[] args) {
+
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         //setting up window and launch screen
-        window = primaryStage;
-        window.setTitle("Snake.exe");
-        window.setScene(launchScreen);
-        window.show();
 
-        StackPane layout = new StackPane();
-        launchScreen  = new Scene(layout, 1920, 1010);
-        Label title = new Label("Welcome to Snake");
-        layout.getChildren().add(title);
-        /*
-        //makes stage title
-        //sets button text
-        window.setTitle("Snake.exe");
-        button.setText("Play Game");
 
-        button.setOnAction(event -> {
+       Label welcome = new Label("Welcome to Snake.exe");
+       Button start = new Button("Start Game");
+       start.setOnAction(e -> {window.setScene(gameScreen);});
+       VBox center = new VBox();
+       center.setAlignment(Pos.BOTTOM_CENTER);
+       center.getChildren().addAll(welcome, start);
+       launchScreen = new Scene(center, 1920, 1040);
 
-        });
 
-        //makes pane
-        //makes button
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        //makes scene
-        //scene dims: 614, 614
-        Scene scene = new Scene(layout, 1920, 1010);
-        */
+       window = primaryStage;
+       window.setTitle("Snake.exe");
+       window.setScene(launchScreen);
+       window.show();
     }
 }
