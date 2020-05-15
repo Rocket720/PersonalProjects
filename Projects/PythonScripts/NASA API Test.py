41a3dc4f97aa892c922573ea6526d97e4d65823e
Rocket720 <Mohapatra06142002>
2020-05-15 10:41:30 -0400

    Aggregated all projects into one repository v2

---
+++ Projects/PythonScripts/NASA API Test.py
@@ -0,0 +1,28 @@
+import json
+import re
+import urllib.request
+import webbrowser
+
+api_key = "cAmgbVyKaw43yBx9UOmCpwFAR4RxEzzwQ8DFebJY"
+
+
+
+#APOD
+apodLink = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
+rawData = json.loads(urllib.request.urlopen(apodLink).read())
+imageURL = rawData["hdurl"]
+title = rawData["title"]
+print(title + ": " + imageURL)
+webbrowser.open(imageURL)
+
+#Earth
+# earthLink =  "https://api.nasa.gov/planetary/earth/assets?lon=41.05&lat=73.69&date=2018-01-01&&dim=0.10&api_key=" + api_key
+# #rawData = json.loads(urllib.request.urlopen(earthLink).read())
+# print(earthLink)
+# #imageURL = rawData["url"]
+# webbrowser.open(imageURL)
+
+
+
+
+
---
+++ Projects/PythonScripts/YTAPITEST.py
@@ -0,0 +1,15 @@
+import json
+import re
+import urllib.request
+
+api_key = "AIzaSyBpJ_NwQpXXBDcOc1jZuZgKDR99h_8_oic"
+channel_id = "UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+
+jsonLink = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key=" + api_key
+rawData = urllib.request.urlopen(jsonLink).read()
+subs = json.loads(rawData)["items"][0]["statistics"]["subscriberCount"]
+views = json.loads(rawData)["items"][0]["statistics"]["viewCount"]
+
+# print(jsonLink)
+print("Subscribers: " + subs + "\nViews: " + views)
+
