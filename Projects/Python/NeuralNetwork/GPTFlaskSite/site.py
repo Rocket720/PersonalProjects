de82da8f2f1e2f0500d8ad86710175d21ac63413
Rocket720 <Mohapatra06142002>
2021-02-16 19:20:27 -0500

    Big Update v2

--- Projects/Python/NeuralNetwork/GPTFlaskSite/site.py
+++ Projects/Python/NeuralNetwork/GPTFlaskSite/site.py
@@ -19,6 +19,6 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", "8080")), debug=True)
+    app.run(host='localhost', port=int(os.getenv("PORT", "8080")), debug=True)
 
 
